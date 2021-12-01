9e29d95751ed5fa839fd89f4c47e4c669a07697a
jordanlsatt <58012718+jordanlsatt@users.noreply.github.com>
2021-11-30 19:49:06 -0500

    7:49

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 connectDB();
 
 // Init middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
